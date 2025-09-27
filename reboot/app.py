457898b4b1d0e0e065e677ba00227b08e3b1dc98
l3L4ck <carlos.gabriel561btu@gmail.com>
2025-09-26 22:23:49 -0300

    criando tela de menu

--- reboot/app.py
+++ reboot/app.py
@@ -15,21 +15,61 @@ app.config['SECRET_KEY'] = 'Senai@791'
 
 # Carrega variáveis de ambiente
 load_dotenv()
-
+# Configurando comunicação via email
 EMAIL_REMETENTE = os.getenv("EMAIL_REMETENTE")
 EMAIL_SENHA = os.getenv("EMAIL_SENHA")
+def callEmail(emailDestina='', remetente='', assunto='', mensagem=''):
+    if emailDestina or remetente or assunto or mensagem:
+        return
+    try:    
+        email = EmailMessage()
+        email["Subject"] = assunto
+        email["From"] = EMAIL_REMETENTE
+        email["To"] = emailDestina
+        corpo = f"Mensagem de: {remetente}\n\n{mensagem}"
+        email.set_content(corpo)
+
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL_REMETENTE, EMAIL_SENHA)
+            smtp.send_message(email)
+
+        resultado = "E-mail enviado com sucesso!"
+    except Exception as e:
+        resultado = f"Erro ao enviar e-mail: {str(e)}"
+
+    return resultado
 
 
-msgRep = False
+# nova função para tratamento de notificações, mas ainda está em teste
+msgAtivo = False
 def msg(mensagem):
-    global msgRep
-    msgRep = not msgRep
-    return mensagem if msgRep else ''
+    global msgAtivo
+    msgAtivo = not msgAtivo
+    return mensagem if msgAtivo else ''
+
+
+data = [
+    {
+        'nome': 'miguel',
+        'dtNas': '2007-08-01',
+        'email': 'pai@gamil.com'
+    },
+    {
+        'nome': 'rodrigo',
+        'dtNas': '2007-08-01',
+        'email': 'pai@gamil.com'
+    },
+]
 
 @app.route('/',methods=["GET", "POST"])
 def inicio ():
+    global data
+    
+    if request.method == "POST":
+        callEmail('carlos.gabriel561btu@gmail.com',"SENAI Testes","Saída e entrada")
 
-    return render_template('index.html', )
+
+    return render_template('autorizacao.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ reboot/static/script/auth.js
@@ -0,0 +1,81 @@
+const at = document.getElementById('atividade')
+at.value = 'Saída'
+function opcao(num = 0) {
+    at.value = num
+}
+const boxCaso = document.querySelectorAll('.boxCaso');
+boxCaso.forEach(n =>
+    n.addEventListener('click', function () {
+        const caso = document.getElementById("caso")
+        caso.style.transform = "translateX(100%)"
+    })
+)
+
+const tagBox = document.getElementsByClassName('tags')[0]
+const tags = ['Trabalho', 'Médico', 'Dentista', 'Outros']
+
+tags.forEach(e => tagBox.innerHTML += `
+    <button class="itemTag" type="button" value="${e}">${e}</button>
+    `)
+
+const itemTag = document.querySelectorAll('.itemTag')
+
+itemTag.forEach(e => e.addEventListener('click', () => {
+    itemTag.forEach(e => { e.style.backgroundColor = '#d7d7e0'; e.style.color = '#000' })
+    e.style.backgroundColor = '#a1a1e0ff'
+    e.style.color = '#fff'
+
+    if (e.value === 'Outros') {
+        document.getElementsByClassName('inp')[4].style.display = 'block'
+        document.getElementsByClassName('inp')[4].value = ''
+    } else {
+        document.getElementsByClassName('inp')[4].style.display = 'none'
+        document.getElementsByClassName('inp')[4].value = e.value
+    }
+}))
+
+
+const inpData = document.getElementById('data')
+const inpTempo = document.getElementById('tempo')
+window.onload = () => {
+    const dataAtual = new Date();
+    const data = dataAtual.toLocaleDateString();
+    const [dia, mes, ano] = data.split("/");
+
+    let hora = `${dataAtual.getHours()}`
+    let min = `${dataAtual.getMinutes()}`
+    inpData.value = `${ano}-${mes}-${dia}`;
+    inpTempo.value = `${hora.padStart(2, "0")}:${min.padStart(2, "0")}`
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let cod = false
+const container = document.getElementsByClassName('container')
+async function teste() {
+    cod = !cod
+
+    if (cod) {
+        container[0].style.transform = `translateX(-104%)`
+        await delay(1000)
+        container[0].style.display = 'none'
+        container[1].style.display = 'grid'
+        await delay(100)
+        container[1].style.transform = `translateY(0)`
+        container[1].style.opacity = 1
+        await delay(300)
+        container[1].classList.add('anima-menu')
+        document.getElementById('title-menu').style.display = 'block'
+        document.getElementById('title-menu').style.opacity = 1
+    } else {
+        container[1].style.transform = `translateY(-104%)`
+        container[1].style.opacity = 0
+        await delay(1000)
+        container[0].style.display = 'block'
+        container[1].style.display = 'none'
+        document.getElementById('title-menu').style.display = 'none'
+        await delay(100)
+        container[0].style.transform = `translateX(0)`
+    }
+}
